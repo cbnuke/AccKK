9860f3707c5656dc93cce59c5db6411dec68701e
CBNUKE <jimmy29922@gmail.com>
2015-08-21 14:51:01 +0700

    Add Font awesome

--- application/views/theme_header.php
+++ application/views/theme_header.php
@@ -8,9 +8,9 @@
         <!-- Bootstrap 3.3.5 -->
         <?= css('bootstrap.min.css') ?>
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <?= css('font-awesome.min.css') ?>
         <!-- Ionicons -->
-        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <!--<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">-->
         <!-- Theme style -->
         <?= css('AdminLTE.min.css') ?>
         <!-- AdminLTE Skins. Choose a skin from the css/skins
